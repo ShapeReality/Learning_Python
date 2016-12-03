5485316718e6777a7a7643d1a96ea08069871706
Taylor L Bartlow <tbartlow@terpmail.umd.edu>
2016-12-02 22:14:02 -0500

    Teaching myself how to code Part:1

---
+++ String_formatting.py
@@ -0,0 +1,7 @@
+name= raw_input("What is your full name?")
+planet= raw_input("What planet will humans colonize next?")
+time= raw_input("How long will that take?")
+
+
+print "%s, colonizing %s sounds impossible. Maybe in %s I will believe it." %(name,planet,time)
+
